70bc49a6f3760c6f2e94a26f246ead41e65b84f7
NLDutchie <maximilius@zeelandnet.nl>
2021-08-21 18:56:05 +0200

    Overhaul

--- script.py
+++ script.py
@@ -1,58 +1,56 @@
 import pytesseract
 from pdf2image import convert_from_path
 from pathlib import Path
-import tempfile
-from PIL import Image
 from docx import Document
 
-#TODO: Error handling for creating images
-#TODO: Tesseract image too large
-#TODO: Find a way to add text to word document instead of txt file
 #TODO: Preprocessing : https://www.freecodecamp.org/news/getting-started-with-tesseract-part-ii-f7f9a0899b3f/
 
-# Create a temp folder to save temp images in
-with tempfile.TemporaryDirectory() as temp_dir:
-  pdfFiles = Path.cwd().glob('*.pdf')
-  for pdfFile in pdfFiles:
-    stemName = str(pdfFile.stem)
-    fileName = str(pdfFile.name)
-    saveDir = Path.cwd() / stemName
-    Path(saveDir).mkdir(parents=True, exist_ok=True)
+# Get all pdf files in corrent directory
+all_pdf_files = Path.cwd().glob('*.pdf')
 
-    # Convert PDF contents to pages
-    pages = convert_from_path(fileName, 500,poppler_path=r"D:\poppler-21.03.0\Library\bin",output_folder=temp_dir)
-    
-    # Create an array of temp images
-    temp_images = []
+# Loop through all the fetched pdf files
+for single_pdf_file in all_pdf_files:
 
-    # Go through the pages
-    for i in range(len(pages)):
-      image_path = f'{temp_dir}/{i}.jpg'
-      pages[i].save(image_path, 'JPEG')
-      temp_images.append(image_path)
-    # read images into pillow.Image
-    imgs = list(map(Image.open, temp_images))
-    # find minimum width of images
-    min_img_width = min(i.width for i in imgs)
-    # find total height of all images
-    total_height = 0
-    for i, img in enumerate(imgs):
-        total_height += imgs[i].height
-    # create new image object with width and total height
-    merged_image = Image.new(imgs[0].mode, (min_img_width, total_height))
-    # paste images together one by one
-    y = 0
-    for img in imgs:
-        merged_image.paste(img, (0, y))
-        y += img.height
-    # save merged image
-    savedImagePath = f'{saveDir}/{stemName}.jpg'
-    merged_image.save(savedImagePath, 'JPEG')
-    processed_image = pytesseract.image_to_string(savedImagePath)
-    f= open(f'{saveDir}/{stemName}.txt',"w+")
-    f.write(processed_image)
-    f.close()
-    document = Document()
-    document.add_paragraph(' ')
-    document.save(f'{saveDir}/{stemName}.docx')
-    
+  # Get only the stem name (e.g. test from test.pdf)
+  pdf_stem_name = str(single_pdf_file.stem)
+
+  # define the path in which a new folder needs to be created to save the pdf images
+  images_save_dir = Path.cwd() / pdf_stem_name
+  
+  # Create a new folder in which the images and docx files will be saved
+  # TODO: Check if folder already exists and if so, just continue instead of overwrite
+  Path(images_save_dir).mkdir(parents=True, exist_ok=True)
+
+  # Convert PDF content to images
+  pdf_images = convert_from_path(single_pdf_file, 500,output_folder=images_save_dir)
+
+  # Define an array in which to fill the image paths (for later, when we will use is for tesseract)
+  saved_images = []
+  
+  # loop through the fetched images of the pdf
+  for i in range(len(pdf_images)):
+
+    # save path
+    saved_image_path = f'{images_save_dir}/{i}.jpg'
+
+    # save the image
+    pdf_images[i].save(saved_image_path,'JPEG')
+
+    # Add the path of the saved image to saved_images
+    saved_images.append(saved_image_path)
+
+  # define an empty string which wil be filled with text from the images
+  image_text = ''
+
+  # Loop through the saved images paths
+  for saved_image in saved_images:
+
+    # Process each image to string
+    processed_image = pytesseract.image_to_string(saved_image)
+    processed_image = processed_image[:-1]
+
+    image_text+=processed_image
+
+  document = Document()
+  document.add_paragraph(image_text)
+  document.save(f'{images_save_dir}/{pdf_stem_name}.docx')
